1d66ee17aba889b65ebaf86e1151f49d45361a47
Sean Marriott <sma462@uclive.ac.nz>
2023-03-13 13:42:51 +1300

    Added genre_types.d.ts

---
+++ src/app/genre_types.d.ts
@@ -0,0 +1,10 @@
+type Genre = {
+    /**
+     * Genre id as defined by the database
+     */
+    genreId: number,
+    /**
+     * Genre's name as entered when created
+     */
+    name: string
+}
