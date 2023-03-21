f96aeb2c800abce00f8a4578ae01d2641e24a5fc
Sean <sma462@uclive.ac.nz>
2023-03-21 15:53:30 +1300

    Implemented password encrypting

--- src/app/models/user.server.model.ts
+++ src/app/models/user.server.model.ts
@@ -1,15 +1,19 @@
 import {ResultSetHeader} from "mysql2";
 import Logger from "../../config/logger";
 import {getPool} from "../../config/db";
+import * as crypto from "crypto";
 
 const insert = async(email: string,
                      firstName: string,
                      lastName: string,
                      password: string): Promise<ResultSetHeader> => {
     Logger.info('Adding user ${email} to the database');
+    const salt = crypto.randomBytes(16).toString('hex');
+    const hash = crypto.pbkdf2Sync(password, salt, 1000, 64, 'sha512').toString('hex');
+
     const conn = await getPool().getConnection();
     const query = 'INSERT INTO user (email, first_name, last_name, password) VALUES (?, ?, ?, ?)'
-    const [ result ] = await conn.query( query, [email, firstName, lastName, password]);
+    const [ result ] = await conn.query( query, [email, firstName, lastName, hash]);
     await conn.release();
     return result;
 }
